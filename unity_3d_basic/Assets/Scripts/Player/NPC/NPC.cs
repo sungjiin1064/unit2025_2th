a029ccc7763c051937158508c59b594330b56c74
sungjiin1064 <psj10645001@gmail.com>
2025-08-29 17:48:57 +0900

    (08-29) 거리를 이용한 AI 로직 구현

---
+++ unity_3d_basic/Assets/Scripts/Player/NPC/NPC.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField] NPCInfo nPCInfo;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D _rigidbody2D;
+    BoxCollider2D boxCollider2D;
+
+
+    [SerializeField] private Vector2 currentTargetPos;
+    [SerializeField] private bool isMoving;
+    Transform playerPos;
+
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+
+        spriteRenderer.sprite = nPCInfo.Sprite;
+        _rigidbody2D.gravityScale = 0;
+
+    }
+    private void Start()
+    {
+        SetRandomPosition();
+    }
+    private void Update()
+    {
+        if(IsPatrol())
+            Patrol();
+        else
+            Chase();
+    }
+
+    bool IsPatrol()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        if(Vector2.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < nPCInfo.patrolDistance)
+            return false;        
+        else
+            return true;
+    }
+
+    public void Patrol()
+    {
+        MoveTargetPoint();
+
+
+
+    }
+
+    public void Chase()
+    {
+        
+        SetPosition(playerPos.position);
+        MoveTargetPoint();
+    }
+
+    private void MoveTargetPoint()
+    {
+        float moveSpeed = UnityEngine.Random.Range((float)nPCInfo.MinSpeed, nPCInfo.MaxSpeed); // using Random = UnityEngine.Random
+
+        if (Vector2.Distance(transform.position, currentTargetPos) < nPCInfo.stopDistance)
+        {
+            _rigidbody2D.velocity = Vector2.zero;
+            isMoving = true;
+
+            //if(isMoving)
+            //    StartCoroutine(SetRandomPositionCoroutine()); //Invoke(nameof(SetRandomPosition), 1f); // À§¶û µ¿ÀÏ
+            if(IsPatrol())
+                SetRandomPosition();
+
+
+
+        }
+        else
+        {
+            _rigidbody2D.velocity = (currentTargetPos - (Vector2)transform.position).normalized * moveSpeed;
+        }
+
+    }
+
+    private void SetRandomPosition()
+    {
+        currentTargetPos = (Vector2)transform.position + UnityEngine.Random.insideUnitCircle * nPCInfo.PatrolRadius;
+    }
+
+    public void SetPosition(Vector2 position)
+    {
+        currentTargetPos = position;
+    }
+
+    private IEnumerator SetRandomPositionCoroutine()
+    {
+        isMoving = false;
+        yield return new WaitForSeconds(1f);
+        SetRandomPosition();
+               
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawChaseCircle();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //DrawChaseCircle();
+    }
+
+    private void  DrawChaseCircle()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, nPCInfo.patrolDistance);
+    }
+
+}
--- unity_3d_basic/Assets/Scripts/Player/NPC/NPCInfo.cs
+++ unity_3d_basic/Assets/Scripts/Player/NPC/NPCInfo.cs
@@ -8,6 +8,8 @@ public class NPCInfo : ScriptableObject
     public int MinSpeed;
     public int MaxSpeed;
     public float PatrolRadius;
+    public float stopDistance = 0.1f;
+    public float patrolDistance = 5f;
     public Sprite Sprite;
     public string NpcName;
 }
